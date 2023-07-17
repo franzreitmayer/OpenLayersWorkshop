62cefdad1d3a705f45ba52bff6b20fa66a5650a4
franz.reitmayer <franz.reitmayer@reitmayer.eu>
2023-07-17 20:57:46 +0200

    drag and drop

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
-import {Map, View} from 'ol';
-import {fromLonLat} from 'ol/proj';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import Link from 'ol/interaction/Link';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
-new Map({
+const map = new Map({
   target: 'map-container',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
   view: new View({
-    center: fromLonLat([0, 0]),
+    center: [0, 0],
     zoom: 2,
-  }),
-})
+  })
+});
+
+map.addInteraction(new Link());
+
+const source = new VectorSource();
+
+const layer = new VectorLayer({
+    source: source,
+  });
+map.addLayer(layer);
+
+map.addInteraction(
+    new DragAndDrop({
+      source: source,
+      formatConstructors: [GeoJSON],
+    })
+  );
+
+
