363036b5594d34b37d8060df5d3f99339b63df5d
franz.reitmayer <franz.reitmayer@reitmayer.eu>
2023-07-17 21:22:08 +0200

    drawing, snaping, download

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ import View from 'ol/View';
 import Link from 'ol/interaction/Link';
 import DragAndDrop from 'ol/interaction/DragAndDrop';
 import Modify from 'ol/interaction/Modify';
+import Draw from 'ol/interaction/Draw';
+import Snap from 'ol/interaction/Snap';
 
 
 const map = new Map({
-  target: 'map-container',
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  })
+    target: 'map-container',
+    view: new View({
+        center: [0, 0],
+        zoom: 2,
+    })
 });
 
 map.addInteraction(new Link());
@@ -22,18 +24,45 @@ const source = new VectorSource();
 
 const layer = new VectorLayer({
     source: source,
-  });
+});
 map.addLayer(layer);
 
 map.addInteraction(
     new DragAndDrop({
-      source: source,
-      formatConstructors: [GeoJSON],
+        source: source,
+        formatConstructors: [GeoJSON],
     })
-  );
+);
 
-  map.addInteraction(
+map.addInteraction(
     new Modify({
-      source: source,
+        source: source,
     })
-  );
+);
+
+map.addInteraction(
+    new Draw({
+        type: 'Polygon',
+        source: source,
+    })
+);
+
+map.addInteraction(
+    new Snap({
+        source: source,
+    })
+);
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+    source.clear();
+});
+
+const format = new GeoJSON({ featureProjection: 'EPSG:3857' });
+const download = document.getElementById('download');
+source.on('change', function () {
+    const features = source.getFeatures();
+    const json = format.writeFeatures(features);
+    download.href =
+        'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+});
