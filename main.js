e0cbf01ed816be514313a77d487094aee107d565
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2023-07-18 10:58:43 +0200

    added ArcGis sattelite map

--- main.js
+++ main.js
@@ -1,68 +1,61 @@
-import GeoJSON from 'ol/format/GeoJSON';
-import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import View from 'ol/View';
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import XYZ from 'ol/source/XYZ.js';
+import OSM from 'ol/source/OSM.js';
 import Link from 'ol/interaction/Link';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
-import Modify from 'ol/interaction/Modify';
-import Draw from 'ol/interaction/Draw';
-import Snap from 'ol/interaction/Snap';
+
+const key = 'RimB45vbiccGjEIpPxR2';
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 
 
-const map = new Map({
-    target: 'map-container',
-    view: new View({
-        center: [0, 0],
-        zoom: 2,
-    })
+
+const aerialLayer = new TileLayer({
+  source: new XYZ({
+    attributions: attributions,
+    url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
+    maxZoom: 20,
+  }),
 });
 
-map.addInteraction(new Link());
+const roadLayer = new TileLayer({
+    source: new XYZ({
+      attributions: attributions,
+      url: 'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=' + key,
+      tileSize: 512,
+      maxZoom: 22,
+    }),
+  });
+  
+  const openMapSatelite = new TileLayer({
+    source: new XYZ( {
+//        url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}",
+        url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+            attribution: "Powered by Esri. " + 
+                "Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community",
+                maxZoom: 24,
+                tileSize: 256 
+                
+        })
+  });
 
-const source = new VectorSource();
 
-const layer = new VectorLayer({
-    source: source,
-});
-map.addLayer(layer);
-
-map.addInteraction(
-    new DragAndDrop({
-        source: source,
-        formatConstructors: [GeoJSON],
-    })
-);
-
-map.addInteraction(
-    new Modify({
-        source: source,
-    })
-);
-
-map.addInteraction(
-    new Draw({
-        type: 'Polygon',
-        source: source,
-    })
-);
-
-map.addInteraction(
-    new Snap({
-        source: source,
-    })
-);
-
-const clear = document.getElementById('clear');
-clear.addEventListener('click', function () {
-    source.clear();
+const view = new View({
+  center: [-6655.5402445057125, 6709968.258934638],
+  zoom: 13,
 });
 
-const format = new GeoJSON({ featureProjection: 'EPSG:3857' });
-const download = document.getElementById('download');
-source.on('change', function () {
-    const features = source.getFeatures();
-    const json = format.writeFeatures(features);
-    download.href =
-        'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+// aerialLayer.setVisible(false);
+const map2 = new Map({
+  target: 'map-container',
+  layers: [
+    openMapSatelite
+
+   ],
+  view: view,
 });
+
+
+map2.addInteraction(new Link());
